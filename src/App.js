1085d223cd638247f63e86bb745bc6c334dda025
jekabskulis <jekabs.kulis@svg.lv>
2024-03-22 15:16:14 +0000

    Added firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Hello World.</h1>
+      {/*
+        <Container className='mb-3' fluid>
+          <Row>
+            {Array.from(cards).map((card, index) =>
+            {
+              return(
+                <Col key={Math.random()} className='m-3 item-list'>
+                  <Form className='checkbox-form'>
+                    <Form.Check type='checkbox' id={`delete-item-${index}`} label="" className='delete-checkbox'></Form.Check>
+                  </Form>
+                </Col>
+              )
+            })}
+          </Row>
+        </Container>
+          */}
     </div>
   );
 }
