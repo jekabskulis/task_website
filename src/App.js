e9b92c351124ba79141ff9a1b4b9163239c130f5
jekabskulis <jekabs.kulis@svg.lv>
2024-03-22 18:23:39 +0000

    Deploying attempt #3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+/*
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
+*/
 function App() {
   return (
     <div className='container'>
